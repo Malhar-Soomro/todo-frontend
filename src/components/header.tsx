d0bb1c06e464ff61b4881fe7857df6f32e416850
Malhar-Soomro <malharsoomro2@gmail.com>
2025-05-12 15:03:38 +0500

    updated header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,41 @@
-// components/Header.tsx
-"use client"
 import Link from "next/link";
+import useAuth from "../hooks/useAuth";
 
 const Header = () => {
-  return (
-    <header className="bg-gray-800 text-white py-4 shadow-md">
-      <div className="container mx-auto px-4 flex justify-between items-center">
-        {/* Left - Logo */}
-        <Link href="/" className="hover:text-gray-400 font-medium">Home</Link>
+  const {isLoggedIn, logout} = useAuth();
 
-        {/* Right - Login / Register */}
-        <div className="w-1/3 text-right space-x-4">
-          <Link href="/login" className="hover:text-gray-400">Login</Link>
-          <Link href="/register" className="hover:text-gray-400">Register</Link>
-        </div>
+ return (
+    <div className="flex justify-between items-center sm:p-2 p-1 px-10 bg-gray-700 shadow-md">
+
+      <div className="pl-4">
+        <Link href={isLoggedIn? "/todos":"/"}>
+          <h1 className="font-bold text-md sm:text-3xl mx-1">
+            MY TODOS
+          </h1>
+        </Link>
       </div>
-    </header>
+
+      <div className="flex items-center gap-2 md:gap-4 py-2 pr-4">
+        {isLoggedIn ? (
+          <>
+          <button className="md:text-sm text-xs cursor-pointer bg-red-500 hover:bg-red-600 px-2 py-1 sm:py-2 sm:px-6 text-white font-semibold rounded-lg transition duration-300 font-poppins" onClick={()=> logout()}>
+              LOGOUT
+            </button>
+          </>
+          
+        ) : (
+          <>
+            <Link className="md:text-sm text-xs cursor-pointer bg-indigo-600 hover:bg-indigo-700 px-2 py-1 sm:px-6 sm:py-2 text-white font-semibold rounded-lg transition duration-300 font-poppins" href="/register">
+              REGISTER
+            </Link>
+            <Link className="md:text-sm text-xs cursor-pointer bg-pink-500 hover:bg-pink-600 px-2 py-1 sm:py-2 sm:px-6 text-white font-semibold rounded-lg transition duration-300 font-poppins" href="/login">
+              SIGN IN
+            </Link>
+          </>
+        )}
+
+      </div>
+    </div>
   );
 };
 
